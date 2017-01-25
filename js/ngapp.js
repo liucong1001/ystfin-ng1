a8569ebdc4c8d27bdd334b00bb2a27820d92fa8a
zhouyang <zhouyang@22f4ba26-3445-4341-af85-382b1e0233a1>
2017-01-25 06:05:47 +0000

    Failed commit: Default

--- js/ngapp.js
+++ js/ngapp.js
@@ -1,12 +1,12 @@
 /**
-    * Created by 扬 on 2016/9/29.
-    */
+ * Created by 扬 on 2016/9/29.
+ */
 require("../css/bootstrap.css")
 require("../css/elements.css")
 require("../css/theme.css")
 require("../css/main.css")
 require("../css/ui.css")
-require("../css/iconfont.css");
+require("../css/iconfont.css")
 
 require("./controller/pay/createOrder")
 require("./controller/pay/queryOrder")
@@ -15,6 +15,12 @@ require("./controller/icard/bind")
 require("./controller/icard/recharge")
 require("./controller/batch/inStep1")
 require("./controller/batch/inStep2")
+require("./controller/trans/sellerStep0")
+require("./controller/trans/sellerStep1")
+require("./controller/trans/sellerStep2")
+require("./controller/trans/sellerStep3")
+
+//require("./controller/test/oss")
 require("./controller/batch/inStep3")
 require("./controller/batch/inStep4")
 require("./controller/icard/details")
