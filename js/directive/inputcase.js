9bac2ebcee4d4f62d978f92399a82b4e85662cdc
zhouyang <zhouyang@22f4ba26-3445-4341-af85-382b1e0233a1>
2017-02-04 03:34:13 +0000

    Failed commit: Default

---
+++ js/directive/inputcase.js
@@ -0,0 +1,24 @@
+/**
+ * Created by 扬 on 2017/1/23.
+ */
+"use strict"
+module.exports = function (app) {
+    app.directive('uppercase', function () {
+        return {
+            require: 'ngModel',
+            link: function (scope, element, attrs, modelCtrl) {
+                var capitalize = function (inputValue) {
+                    if (inputValue == undefined) inputValue = '';
+                    var capitalized = inputValue.toUpperCase();
+                    if (capitalized !== inputValue) {
+                        modelCtrl.$setViewValue(capitalized);
+                        modelCtrl.$render();
+                    }
+                    return capitalized;
+                }
+                modelCtrl.$parsers.push(capitalize);
+                capitalize(scope[attrs.ngModel]); // capitalize initial value
+            }
+        }
+    })
+}
